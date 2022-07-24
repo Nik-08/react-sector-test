d17f1fb6c198a51fb5e70ed059e06597f4f37944
Nik <nikitos08.1@mail.ru>
2022-07-25 01:15:28 +0700

    add github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { App } from "./App";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
@@ -10,11 +11,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
